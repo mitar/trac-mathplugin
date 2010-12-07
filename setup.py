573810e0cd0835d6afa5041853370c3f0207b4d8
Kamil Kisiel <kamil@kamilkisiel.net>
2010-12-06 22:38:08 -0800

    Updated setup.py with maintainer name and email

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-        name='TracMath', version='0.1',
-        packages=find_packages(exclude=['*.tests*']),
-        entry_points="""
-                    [trac.plugins]
-                    tracmath = tracmath
-                    """,
-    )
-
+    name='TracMath', version='0.3',
+    maintainer="Kamil Kisiel",
+    maintainer_email="kamil@kamilkisiel.net",
+    packages=find_packages(exclude=['*.tests*']),
+    entry_points="""
+    [trac.plugins]
+    tracmath = tracmath
+    """,
+)
